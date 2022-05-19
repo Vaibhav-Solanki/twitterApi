6068e599b392ea83c25abd19c6b3e91659c51095
Vaibhav Solanki <vaibhavsolankie@gmail.com>
2022-05-19 09:47:07 +0530

    git init

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// 1. Import the extendTheme function
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
+  </React.StrictMode>
+);
