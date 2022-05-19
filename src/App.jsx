0a2a481bba511e1ca96e0f9699919032ec990bc6
Vaibhav Solanki <vaibhavsolankie@gmail.com>
2022-05-19 17:01:49 +0530

    twitUi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
 import Card from "./components/Card";
 
+import twitter from "./config";
+
 export default function App() {
+  useEffect(() => {
+    getDta();
+    return () => {
+      console.log("unmount");
+    };
+  }, []);
+  const getDta = async () => {
+    try {
+      let res = await fetch("https://api.twitter.com/2/tweets/search/all", {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        // mode: "no-cors",
+        headers: {
+          Authorization: twitter.BEARER,
+        },
+      });
+      let data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return <Card />;
 }
